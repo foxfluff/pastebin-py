242a6b09076f7b9e2cea73cc25c800cc60217747
foxfluff <foxfluff@users.noreply.github.com>
2016-10-25 08:44:32 -0400

    Implemented simple create_paste function, and placeholder for paste class.

---
+++ pastebin.py
@@ -0,0 +1,38 @@
+
+import urllib
+import urllib2
+import json
+
+API_DEV_KEY = "00000000"
+API_USER_KEY = "00000000"
+API_PASTE_PUBLIC = 0
+API_PASTE_UNLISTED = 1
+API_PASTE_PRIVATE = 2
+
+class paste(object):
+    pass
+
+def create_paste(text, dev_key = API_DEV_KEY, paste_privacy = None,
+                 user_key = None, name = None, format = None,
+                 expire_date = None):
+    
+    req = urllib2.Request("http://pastebin.com/api/api_post.php")
+    data = {"api_option": "paste",
+            "api_dev_key": dev_key,
+            "api_paste_code": text}
+    
+    if paste_privacy:
+        data["api_paste_private"] = paste_privacy
+    if user_key:
+        data["api_user_key"] = user_key
+    if name:
+        data["api_paste_name"] = name
+    if format:
+        data["api_paste_format"] = format
+    if expire_date:
+        data["api_paste_expire_date"] = expire_date
+        
+    req.add_data(urllib.urlencode(data))
+    response = urllib2.urlopen(req)
+    
+    return response.read()
